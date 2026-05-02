73384d52413de43e1de2b0cf0307847f99763c65
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2026-05-02 04:10:44 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
 echo "==> Running migrations..."
-bundle exec rails db:migrate
+bundle exec rails db:migrate --trace
+echo "==> Migration exit code: $?"
 echo "==> Starting server on port $PORT..."
 bundle exec rails server -b 0.0.0.0 -p $PORT
