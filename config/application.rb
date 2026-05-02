5b94d6ea467c7a8f48c940ebfbe167ddad67db80
afrocan1 <114925535+afrocan1@users.noreply.github.com>
2026-05-02 02:25:08 +0000

    Update host configurations in application.rb
    Add additional host configurations for the application.

--- config/application.rb
+++ config/application.rb
@@ -1,36 +1,21 @@
 require_relative "boot"
-
 require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module RauversionRor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
     config.exceptions_app = routes
-
     config.hosts << "chaskiq.sa.ngrok.io"
     config.hosts << ENV["HOST"]
-
+    config.hosts << ENV["APP_DOMAIN"]
+    config.hosts << ENV["DOMAIN"]
     config.mission_control.jobs.http_basic_auth_enabled = false
     config.mission_control.jobs.base_controller_class = "Admin::JobsController"
-
     config.hosts << "www.example.com" if Rails.env.test?
-
     config.active_storage.track_variants = true
-
     config.autoload_paths << Rails.root.join('lib')
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
